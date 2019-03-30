ab310d586353b28480de6e86766622c49b806cd6
M1M0G <M1M0G@github.com>
2019-03-30 19:42:52 +0500

    Добавьте файлы проекта.

---
+++ HotelHubData/HotelHubData.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotelHubData
+{
+    /// <summary>
+    /// Бронирование номера
+    /// </summary>
+    public class Booking
+    {
+
+        /// <summary>
+        /// Выбор номера
+        /// </summary>
+        public List<Rooms> GetRooms { get; set; }
+
+        /// <summary>
+        /// Отмена бронирования
+        /// </summary>
+        public string CancelBooking { get; set; }
+
+}
+
+
+    /// <summary>
+    /// Номера 
+    /// </summary>
+    public class Rooms
+    {
+        /// <summary>
+        /// Дата заезда и выезда 
+        /// </summary>
+        public DateTime Residency { get; set; }
+        
+        /// <summary>
+        /// Количество человек
+        /// </summary>
+        public int NumberOfPersons { get; set; }
+
+        /// <summary>
+        /// Компановка спальных мест  
+        /// </summary>
+        public int NumberOfBeds { get; set; }
+
+        /// <summary>
+        /// Фотография номера
+        /// </summary>
+        public byte[] Photo { get; set; }
+    }
+
+    /// <summary>
+    /// Начальная страница
+    /// </summary>
+    public class StartPage
+    {
+        /// <summary>
+        /// Новости Отеля
+        /// </summary>
+        public string News { get; set; }
+
+        /// <summary>
+        /// Авторизация
+        /// </summary>
+        public List<Autorization> Autorization { get; set; }
+    }
+
+    /// <summary>
+    /// Авторизация
+    /// </summary>
+    public class Autorization
+    {
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Пароль
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Почта
+        /// </summary>
+        public string Mail { get; set; }
+    }
+
+    /// <summary>
+    /// Обратная связь
+    /// </summary>
+    public class FeedBack
+    {
+        /// <summary>
+        /// Сообщение пользователя
+        /// </summary>
+        public string User { get; set; }
+
+        /// <summary>
+        /// Письмо от Администратора/Сообщение от Администратора
+        /// </summary>
+        public string Admin { get; set; }
+    }
+}
